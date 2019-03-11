f525b9a59066e8870a2cab01ad55a18774271ad8
CelaVanilla <47067970+CelaVanilla@users.noreply.github.com>
2019-03-10 23:49:23 -0400

    Add files via upload

---
+++ Challenge 2 Scripts/Audio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    public AudioSource musicSource;
+
+    void Start()
+    {
+        musicSource.clip = musicClipOne;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            musicSource.Stop();
+
+        }
+    }
+}
---
+++ Challenge 2 Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+    public float jumpForce;
+    public Text winText;
+    public Text loseText;
+    public Text hp;
+    public Text scoreText;
+    private int health = 3;
+    private int score = 0;
+    private GameObject player;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+        winText.text = "";
+        loseText.text = "";
+        SetScoreText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            score = score + 1;
+            SetScoreText();
+            Health();
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            health = health - 1;
+            Health();
+        }
+
+    }
+
+    void Health()
+    {
+        if (health == 0)
+        {
+            loseText.text = "You Lose...";
+            player = GameObject.FindWithTag("Player");
+            player.gameObject.SetActive(false);
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        hp.text = "Health: " + health.ToString() + " HP";
+
+        if (score == 4)
+        {
+            health = 3;
+            transform.position = -new Vector2(-41.25f, 0f);
+        }
+        if (score >= 8)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
